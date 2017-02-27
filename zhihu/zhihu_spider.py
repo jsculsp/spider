1b748c4903b52c3bf29d4a882243e58d33b0f8b7
jsculsp <328232234@qq.com>
2017-02-27 11:09:49 +0800

    save

--- zhihu/zhihu_spider.py
+++ zhihu/zhihu_spider.py
@@ -10,33 +10,38 @@ from ..log import log
 PROJECT_ROOT = os.path.normpath(os.path.dirname(os.path.dirname(__file__)))
 sys.path.insert(1, PROJECT_ROOT)
 
+headers = {
+    'Host': 'www.zhihu.com',
+    'Connection': 'keep-alive',
+    'Cache-Control': 'max-age=0',
+    'Accept': '*/*',
+    'Origin': 'https://www.zhihu.com',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.154 Safari/537.36 LBBROWSER',
+    'X-Requested-With': 'XMLHttpRequest',
+    'Referer': 'https://www.zhihu.com/',
+    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+}
+
 
 def generate_opener():
     filename = os.path.join(PROJECT_ROOT, 'zhihu/cookie.txt')
-    cookie = cookielib.MozillaCookieJar(filename)
+    try:
+        cookie = cookielib.MozillaCookieJar()
+        cookie.load(filename, ignore_discard=True, ignore_expires=True)
+    except:
+        cookie = cookielib.MozillaCookieJar(filename)
     opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookie))
     return cookie, opener
 
 
-def login():
+def login(password):
     cookie, opener = generate_opener()
     postdata = urllib.urlencode({
         'email': 'jsculsp@126.com',
-        'password': 'wwg1984',
+        'password': password,
         '_xsrf': 'f2d467cceb2be896f51a69b6db691810',
         'captcha_type': 'cn',
     })
-    headers = {
-        'Host': 'www.zhihu.com',
-        'Connection': 'keep-alive',
-        'Cache-Control': 'max-age=0',
-        'Accept': '*/*',
-        'Origin': 'https://www.zhihu.com',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.154 Safari/537.36 LBBROWSER',
-        'X-Requested-With': 'XMLHttpRequest',
-        'Referer': 'https://www.zhihu.com/',
-        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
-    }
     loginUrl = 'https://www.zhihu.com/login/email'
     req = urllib2.Request(loginUrl, data=postdata, headers=headers)
     try:
@@ -48,21 +53,11 @@ def login():
     return True
 
 
-def visit_by_url(url, filename):
-    flag = login()
+def visit_by_url(password, url, filename):
+    flag = login(password)
     if flag == False:
         print('login failed, please check your log file...')
     _, opener = generate_opener()
-    headers = {
-        'Host': 'www.zhihu.com',
-        'Connection': 'keep-alive',
-        'Cache-Control': 'max-age=0',
-        'Accept': '*/*',
-        'Origin': 'https://www.zhihu.com',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.154 Safari/537.36 LBBROWSER',
-        'X-Requested-With': 'XMLHttpRequest',
-        'Referer': 'https://www.zhihu.com/',
-    }
     req = urllib2.Request(url, headers=headers)
     result = opener.open(req)
     path = os.path.join(PROJECT_ROOT, 'zhihu/pages', filename)
